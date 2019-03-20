4d18e97da8730c902834abbf220f972a41333de9
Murat Toygar <toygar-murat@hotmail.com>
2019-03-20 23:12:23 +0300

    add ember-cli-concat configs

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,18 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    fingerprint: {
+      exclude: ['app']
+    },
+    storeConfigInMeta: false,
+    emberCliConcat: {
+      js: {
+        concat: true
+      },
+      css: {
+        concat: true
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
