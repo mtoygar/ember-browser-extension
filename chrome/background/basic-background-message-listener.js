6cfa1b4d29368b69cccb3dde6327246ceb306efe
Murat Toygar <toygar-murat@hotmail.com>
2019-04-23 15:48:24 +0300

    migrate browser script to browser-polyfill

--- chrome/background/basic-background-message-listener.js
+++ chrome/background/basic-background-message-listener.js
@@ -1,6 +1,6 @@
 const MAGICAL_THRESHOLD = 0.372;
 
-chrome.runtime.onMessage.addListener(messageListener);
+browser.runtime.onMessage.addListener(messageListener);
 
 function messageListener(request, sender, sendResponse) {
   let response = Math.random() > MAGICAL_THRESHOLD ? 'Hi, Zoey!' : 'Hello, Tomster!';
