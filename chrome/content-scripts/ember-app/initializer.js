50b52c5830e2f3a697ea845bba35654da6a965fa
Murat Toygar <toygar-murat@hotmail.com>
2020-11-01 23:08:37 +0300

    fix content script errors stem from github UI change

--- chrome/content-scripts/ember-app/initializer.js
+++ chrome/content-scripts/ember-app/initializer.js
@@ -1,27 +1,33 @@
 // import { GITHUB_BUTTON_ID, EMBER_EXTENSION_LOCATION_HASH_PREFIX, EMBER_ROOT_ELEMENT_SELECTOR } from './constants';
 
 function addEmberTriggerButtonToDOM() {
-  let githubNavigationBar = document.querySelector('.reponav');
-  let triggerButton = createTriggerButton();
+  const githubNavigationBar = document.querySelector('.js-repo-nav > ul');
+  const triggerButton = createTriggerButton();
   githubNavigationBar.appendChild(triggerButton);
 }
 
 function createTriggerButton() {
-  let elem = document.createElement('a');
+  const listElem = document.createElement('li');
+  listElem.classList = 'd-flex';
+
+  const elem = document.createElement('a');
   elem.id = GITHUB_BUTTON_ID;
   elem.text = 'Start Ember';
   elem.classList = 'js-selected-navigation-item reponav-item';
   elem.href = EMBER_EXTENSION_LOCATION_HASH_PREFIX;
   elem.onclick = runEmber;
-  return elem;
+
+  listElem.appendChild(elem);
+
+  return listElem;
 }
 
 function runEmber(){
   cleanGithubContainer();
   addEmberContainerToGithubContainer();
-  let App = require('ember-browser-extension/app')['default'].create({ autoboot: false });
+  const App = require('ember-browser-extension/app')['default'].create({ autoboot: false });
 
-  let options = {
+  const options = {
     // Override the router's location adapter to prevent it from updating the URL in the address bar
     location: 'hash',
 
@@ -35,14 +41,14 @@ function runEmber(){
 }
 
 function cleanGithubContainer(){
-  let githubContainer = document.querySelector('.container.new-discussion-timeline');
-  let childNodeToRemove = githubContainer.querySelector('div.repository-content');
+  const githubContainer = document.querySelector('.new-discussion-timeline');
+  const childNodeToRemove = githubContainer.querySelector('div.repository-content');
   githubContainer.removeChild(childNodeToRemove);
 }
 
 function addEmberContainerToGithubContainer(){
-  let githubContainer = document.querySelector('.container.new-discussion-timeline');
-  let emberContainer = document.createElement('div');
+  const githubContainer = document.querySelector('.new-discussion-timeline');
+  const emberContainer = document.createElement('div');
   emberContainer.id = 'content-script-selector';
   githubContainer.appendChild(emberContainer);
 }
